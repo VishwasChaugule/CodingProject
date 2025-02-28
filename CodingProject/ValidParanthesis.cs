7dd4d5ae8bf75284bdc047dc811cdd01703bb61a
Vishwas Chaugule <chaugule1369vish@gmail.com>
2025-02-28 11:50:24 +0530

    add valid paranthesis program

---
+++ CodingProject/ValidParanthesis.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace CodingProject
+{
+	public class ValidParanthesis
+	{
+		public ValidParanthesis()
+		{
+		}
+
+		public static bool CheckValidParanthesis(string input)
+		{
+			Stack<char> openingParanthesis = new Stack<char>();
+			foreach (var character in input)
+			{
+				if (character == '(' || character == '[' || character == '{')
+				{
+					openingParanthesis.Push(character);
+				}
+
+				if(character == ')' || character == ']' || character == '}')
+				{
+					if (openingParanthesis.Count() == 0)
+						return false;
+					var topElement = openingParanthesis.Peek();
+					if ((character == ')' && topElement == '(') || (character == ']' && topElement == '[')
+						|| (character == '}' && topElement == '{'))
+					{
+						openingParanthesis.Pop();
+					}
+					else
+					{
+						return false;
+					}
+				}
+			}
+
+			if (openingParanthesis.Count() == 0)
+				return true;
+			return false;
+		}
+	}
+}
+
