bbcac5e91a6e43b14950ec71234b7435e2529529
Vishwas Chaugule <chaugule1369vish@gmail.com>
2025-03-01 10:23:49 +0530

    add reverse string program

---
+++ CodingProject/ReverseString.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace CodingProject
+{
+	public class ReverseString
+	{
+		public ReverseString()
+		{
+		}
+
+        public static string Reverse(string originalString)
+		{
+			StringBuilder reversedString = new StringBuilder();
+
+			for (int i = originalString.Length-1; i >= 0; i--)
+			{
+				reversedString.Append(originalString[i]);
+			}
+
+			return reversedString.ToString();
+		}
+	}
+}
+
