16d7c6284fc22f23a9d04eee5e0355ee8aa0b23f
Vishwas Chaugule <chaugule1369vish@gmail.com>
2025-03-01 16:14:08 +0530

    Palidrome program

---
+++ CodingProject/Palindrome.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace CodingProject
+{
+	public class Palindrome
+	{
+		public Palindrome()
+		{
+		}
+
+		public static bool IsPalindrome(string text)
+		{
+			string reversedString = ReverseString.Reverse(text);
+
+			return text.ToLower() == reversedString.ToLower();
+		}
+	}
+}
+
