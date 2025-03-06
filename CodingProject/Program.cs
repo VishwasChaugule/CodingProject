f7ceb5c5d48ce78362ce40c4405b85d998780e55
Vishwas Chaugule <chaugule1369vish@gmail.com>
2025-03-06 05:59:57 +0530

    add remove duplicate characters program

--- CodingProject/Program.cs
+++ CodingProject/Program.cs
@@ -26,3 +26,15 @@ foreach (var num in twosum2)
 }
 
 Console.WriteLine(FindMissingNumber.FindMissingNumberInArray(new int[] { 1, 3, 4, 5 }));
+
+Console.WriteLine(ValidParanthesis.CheckValidParanthesis("(({}[]))"));
+Console.WriteLine(ValidParanthesis.CheckValidParanthesis("(({[]()))}"));
+
+Console.WriteLine(ReverseString.Reverse("racecar"));
+Console.WriteLine(ReverseString.Reverse("string"));
+
+Console.WriteLine(Palindrome.IsPalindrome("RaceCar"));
+Console.WriteLine(Palindrome.IsPalindrome("madam"));
+Console.WriteLine(Palindrome.IsPalindrome("hello"));
+
+Console.WriteLine(RemoveDuplicateCharacters.RemoveDuplicateCharactersFromString("aabbbcc"));
---
+++ CodingProject/RemoveDuplicateCharacters.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace CodingProject
+{
+	public class RemoveDuplicateCharacters
+	{
+		public RemoveDuplicateCharacters()
+		{
+		}
+
+		public static string RemoveDuplicateCharactersFromString(string inputString)
+		{
+			string result = string.Empty;
+
+			for (int i = 0; i < inputString.Length; i++)
+			{
+				if (!result.Contains(inputString[i]))
+				{
+					result += inputString[i];
+				}
+			}
+
+			return result;
+		}
+	}
+}
+
