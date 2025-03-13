cb44ed856e7a5d14ac92498ee25fe0127d22b053
Vishwas Chaugule <chaugule1369vish@gmail.com>
2025-03-13 05:59:46 +0530

    update max subarray program

--- CodingProject/MaxSubArraySum.cs
+++ CodingProject/MaxSubArraySum.cs
@@ -10,7 +10,7 @@ namespace CodingProject
 		public static int GetMaxSubArraySum(int[] nums)
 		{
 			int maxSumSoFar = nums[0], maxSumEndingHere = nums[0];
-			for (int i = 0; i < nums.Length; i++)
+			for (int i = 1; i < nums.Length; i++)
 			{
 				maxSumEndingHere = Math.Max(nums[i], maxSumEndingHere + nums[i]);
 				maxSumSoFar = Math.Max(maxSumSoFar, maxSumEndingHere);
